dd5096ecc4b176c17503829fbcb74a0391b1c605
Victor Lima <vlima@wtecenergy.com>
2025-02-18 21:30:12 -0300

    Add sonar quality gate param

--- Tests/Controllers/PaymentControllerTests.cs
+++ Tests/Controllers/PaymentControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using GestaoPedidosPayment.AppServices.Shared;
+﻿using FluentAssertions;
+using GestaoPedidosPayment.AppServices;
+using GestaoPedidosPayment.AppServices.Shared;
 using GestaoPedidosPayment.AppServices.Shared.Dtos;
 using GestaoPedidosPayment.Controllers;
 using Microsoft.AspNetCore.Mvc;
@@ -123,6 +125,37 @@ namespace Tests.Controllers
             // Assert
             Assert.IsType<OkResult>(result);
             await _paymentAppService.Received(1).SetCompleted(paymentId);
+        }
+
+        [Fact(DisplayName = "GIVEN a valid CreatePaymentDto WHEN CreatePayment is called THEN it should return CreatedAtAction with payment details")]
+        public async Task Given_Valid_CreatePaymentDto_When_CreatePayment_Called_Then_Should_Return_CreatedAtAction()
+        {
+            // GIVEN a valid CreatePaymentDto
+            var createPaymentDto = new CreatePaymentDto
+            {
+                OrderId = "ORDER123",
+                Amount = 99.99m
+            };
+
+            var paymentDto = new PaymentDto
+            {
+                Id = Guid.NewGuid(),
+                OrderId = createPaymentDto.OrderId,
+                Amount = createPaymentDto.Amount
+            };
+
+            _paymentAppService.CreatePayment(createPaymentDto).ReturnsForAnyArgs(paymentDto);
+
+            // WHEN CreatePayment is called
+            var result = await _controller.CreatePayment(createPaymentDto);
+
+            // THEN it should return CreatedAtAction with payment details
+            result.Should().BeOfType<CreatedAtActionResult>();
+
+            var createdAtResult = result as CreatedAtActionResult;
+            createdAtResult!.ActionName.Should().Be(nameof(PaymentController.GetPayment));
+            createdAtResult.RouteValues!["id"].Should().Be(paymentDto.Id);
+            createdAtResult.Value.Should().BeEquivalentTo(paymentDto);
         }
     }
 }
